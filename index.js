616f9b1e8a5877993f857649d19f424081c2626f
brian51665 <bdavis51892@yahoo.com>
2017-10-02 22:19:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ const facts = [
   var i = 0;
 
 function johnLennonFacts(Array) {
+  const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ];
+  var i = 0;
   while (i < 4){
     facts[i] = facts[i] + "!!!";
     i++;
